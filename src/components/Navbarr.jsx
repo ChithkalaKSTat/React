3d9021f879819cbf895d48ec648cf2bba931f1ee
ChithkalaKSTat <thisisecacc@gmail.com>
2025-08-24 20:40:56 +0530

    Update Navbarr.jsx

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -7,7 +7,7 @@ export default function Navbarr(props) {
   return (
      <nav className={`navbar navbar-expand-lg  navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">{props.title}</a>
+    <a className="navbar-brand" href="/">{props.title}</a>
 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>     
     </button>
@@ -17,7 +17,7 @@ export default function Navbarr(props) {
           <a className="nav-link active" aria-current="page"  href ="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link"  href ="#">{props.abouttext}</a>
+          <a className="nav-link"  href ="/">{props.abouttext}</a>
         </li>
 
        
